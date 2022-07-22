4bdbdf0d20decf2f33237f9d532fd1e6e69333d9
danradic <danradic@gmail.com>
2022-07-22 14:57:19 +0200

    add Pie, Category models and repositories

---
+++ src/BethanysPieShop.Website/Models/ICategoryRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace BethanysPieShop.Website.Models
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> AllCategories { get; }
+    }
+}
---
+++ src/BethanysPieShop.Website/Models/IPieRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace BethanysPieShop.Website.Models
+{
+    public interface IPieRepository
+    {
+        IEnumerable<Pie> AllPies { get; }
+        IEnumerable<Pie> PiesOfTheWeek { get; }
+        Pie? GetPieById(int pieId);
+    }
+}
