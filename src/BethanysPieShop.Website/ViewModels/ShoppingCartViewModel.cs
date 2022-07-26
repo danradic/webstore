8556d8a7bf79e66369168651e19da8f98da29077
danradic <danradic@gmail.com>
2022-07-26 20:26:05 +0200

    add ShoppingCartController and ShoppingCartViewModel

---
+++ src/BethanysPieShop.Website/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,16 @@
+﻿using BethanysPieShop.Website.Models;
+
+namespace BethanysPieShop.Website.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public ShoppingCartViewModel(IShoppingCart shoppingCart)
+        {
+            ShoppingCart = shoppingCart;
+            ShoppingCartTotal = shoppingCart.GetShoppingCartTotal();
+        }
+
+        public IShoppingCart ShoppingCart { get; }
+        public decimal ShoppingCartTotal { get; set; }
+    }
+}
