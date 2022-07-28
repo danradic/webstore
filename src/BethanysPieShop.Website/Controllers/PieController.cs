e93e420d175426bd456baa57fdcd02f34117426c
danradic <danradic@gmail.com>
2022-07-28 11:36:34 +0200

    consolidate pie list action logic

--- src/BethanysPieShop.Website/Controllers/PieController.cs
+++ src/BethanysPieShop.Website/Controllers/PieController.cs
@@ -18,9 +18,22 @@ namespace BethanysPieShop.Website.Controllers
 
         public IActionResult List(string category) 
         {
-            var pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category);
+            IEnumerable<Pie> pies = _pieRepository.AllPies.OrderBy(p => p.Name);
+            string? currentCategory;
 
-            return View(new PieListViewModel(pies, category));
+            if (pies == null || !pies.Any()) return NotFound();
+
+            if (string.IsNullOrEmpty(category))
+            {
+                currentCategory = "All pies";
+                return View(new PieListViewModel(pies, currentCategory)); 
+            }
+
+            pies = pies.Where(p => p.Category.CategoryName == category);
+            currentCategory = pies.FirstOrDefault(p => p.Category.CategoryName == category)?
+                .Category.CategoryName;
+
+            return View(new PieListViewModel(pies, currentCategory));
         }
 
         public IActionResult Details(int id) 
