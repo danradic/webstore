9fb278d9fbdc4716a84f547969142be09564c4ec
danradic <danradic@gmail.com>
2022-07-29 13:59:42 +0200

    add unit tests for PieController

---
+++ tests/BethanysPieShop.UnitTests/Controllers/PieControllerTests.cs
@@ -0,0 +1,54 @@
+﻿using BethanysPieShop.UnitTests.Mocks;
+using BethanysPieShop.Website.Controllers;
+using BethanysPieShop.Website.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.UnitTests.Controllers
+{
+    public class PieControllerTests
+    {
+        [Fact]
+        public void List_ShouldReturnAllPies_WhenCategoryParameterIsEmpty() 
+        {
+            // Arrange
+            var pieRepositoryMock = RepositoryMocks.GetPieRepositoryMock();
+            var categoryRepositoryMock = RepositoryMocks.GetPCatergoryRepositoryMock();
+
+            var pieController = new PieController(pieRepositoryMock.Object, categoryRepositoryMock.Object);
+
+            // Act
+            var result = pieController.List("");
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var pieListViewModel = Assert.IsAssignableFrom<PieListViewModel>(viewResult.ViewData.Model);
+            Assert.Equal(10, pieListViewModel.Pies.Count());
+        }
+
+        [Theory]
+        [InlineData("Fruit pies")]
+        [InlineData("Cheese cakes")]
+        [InlineData("Seasonal pies")]
+        public void List_ShouldReturnPiesByCategory_WhenCategoryParameterIsNotEmpty(string category) 
+        {
+            // Arrange
+            var pieRepositoryMock = RepositoryMocks.GetPieRepositoryMock();
+            var categoryRepositoryMock = RepositoryMocks.GetPCatergoryRepositoryMock();
+
+            var pieController = new PieController(pieRepositoryMock.Object, categoryRepositoryMock.Object);
+
+            // Act
+            var result = pieController.List(category);
+
+            //Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var pieListViewModel = Assert.IsAssignableFrom<PieListViewModel>(viewResult.ViewData.Model);
+            Assert.True(pieListViewModel.Pies.All(p => p.Category.CategoryName == category));
+        }
+    }
+}
