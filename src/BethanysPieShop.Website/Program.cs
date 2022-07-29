6899e6049f09e5ba82e0ea881688f42ef946b651
danradic <danradic@gmail.com>
2022-07-29 19:43:47 +0200

    add pie search Web APIs

--- src/BethanysPieShop.Website/Program.cs
+++ src/BethanysPieShop.Website/Program.cs
@@ -1,10 +1,15 @@
 using BethanysPieShop.Website.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+    .AddJsonOptions(options => 
+    {
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    });
 builder.Services.AddRazorPages();
 
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
